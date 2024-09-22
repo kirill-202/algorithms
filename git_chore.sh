a0e56a72d27eb2ee29b2f3242c89465ba90074f9
Kirill <kirillgoodline@gmail.com>
2024-09-23 01:41:06 +0300

    update the success message

--- git_chore.sh
+++ git_chore.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 
-
 if [ $# -eq 0 ]; then
     echo "Error: Provide commit message"
     exit 1
@@ -9,4 +8,5 @@ fi
 
 current_branch=$(git branch --show-current)
 
-git add . && git commit -m "$1" && git push origin "$current_branch" && echo success
+git add . && git commit -m "$1" && git push origin "$current_branch" && \
+echo "Success: GitHub repository has been updated"
